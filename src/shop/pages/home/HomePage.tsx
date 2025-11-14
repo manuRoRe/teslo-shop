cef87f9d10600bc6a8c2900b229d5b5782649ff3
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-14 12:31:04 +0100

    install of  shadcn, taillwind and a font from google fonts

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="font-montserrat">Home</h1>
+      <h1 className="font-montserrat font-bold">Home</h1>
+      <Button>Holaa Mundooo</Button>
+    </div>
+  );
 };
