c4eb7cbf9938ccc94a17271764d05a9266b8c48a
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-17 11:29:49 +0100

    dev the logic of the inputSearch

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,10 +1,31 @@
 import { Search, ShoppingBag, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useSearchParams } from "react-router";
 
 const CustomHeader = () => {
   const [cartCount] = useState(3);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const query = searchParams.get("query") || "";
+
+  const handleinputSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== "Enter") {
+      return;
+    }
+    const newSearchParams = new URLSearchParams();
+
+    const query = inputRef.current?.value;
+    if (!query) {
+      newSearchParams.delete("query");
+    } else newSearchParams.set("query", query);
+
+    setSearchParams(newSearchParams);
+  };
+
   return (
     <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
       <div className="container mx-auto px-4 lg:px-8">
@@ -54,7 +75,10 @@ const CustomHeader = () => {
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
                 <Input
                   placeholder="Buscar productos..."
-                  className="pl-9 w-64 h-9"
+                  className="pl-9 w-64 h-9 bg-white"
+                  ref={inputRef}
+                  onKeyDown={(event) => handleinputSearch(event)}
+                  defaultValue={query}
                 />
               </div>
             </div>
