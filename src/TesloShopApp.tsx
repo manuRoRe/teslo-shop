24605c574d94492ea202024b4e40762857c75098
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-18 10:22:24 +0100

    Add React Query for data fetching and state management; integrate QueryClientProvider and ReactQueryDevtools

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,6 +1,17 @@
 import { RouterProvider } from "react-router";
 import { appRouter } from "./app.router";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export const TesloShopApp = () => {
-  return <RouterProvider router={appRouter}></RouterProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <RouterProvider router={appRouter}></RouterProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
