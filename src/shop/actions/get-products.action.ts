8fb519ad3491eb998eda0845375daa1cd320f4c6
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-18 11:09:04 +0100

    map the images in getProductsAction

--- src/shop/actions/get-products.action.ts
+++ src/shop/actions/get-products.action.ts
@@ -1,7 +1,20 @@
 import { tesloApi } from "@/api/teslo-api";
 import type { ProductsResponse } from "@/interfaces/products.response";
 
-export const getProductsAction = async () => {
+// localhost:3000/api/files/product/1703767-00-A_0_2000.jpg
+
+export const getProductsAction = async (): Promise<ProductsResponse> => {
   const { data } = await tesloApi.get<ProductsResponse>("/products");
-  return data;
+
+  const productsWithImageUrl = data.products.map((p) => ({
+    ...p,
+    images: p.images.map(
+      (img) => `${import.meta.env.VITE_API_URL}/files/product/${img}`
+    ),
+  }));
+
+  return {
+    ...data,
+    products: productsWithImageUrl,
+  };
 };
