34bdd6d90f017e603549d4c599594246883dfac2
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-18 10:49:08 +0100

    Add type annotation for ProductsResponse in getProductsAction

--- src/shop/actions/get-products.action.ts
+++ src/shop/actions/get-products.action.ts
@@ -1,6 +1,7 @@
 import { tesloApi } from "@/api/teslo-api";
+import type { ProductsResponse } from "@/interfaces/products.response";
 
 export const getProductsAction = async () => {
-  const { data } = await tesloApi.get("/products");
+  const { data } = await tesloApi.get<ProductsResponse>("/products");
   return data;
 };
