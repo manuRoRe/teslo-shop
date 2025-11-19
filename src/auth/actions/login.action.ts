51d6cc6785c009e12e5131421d45f2abe4096a75
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-19 13:24:48 +0100

    create the login action and the AuthResponse to match the data

---
+++ src/auth/actions/login.action.ts
@@ -0,0 +1,26 @@
+import { tesloApi } from "@/api/teslo-api";
+import type { AuthResponse } from "../interfaces/auth.response";
+
+interface loginOptins {
+  email: string;
+  password: string;
+}
+
+export const loginAction = async ({
+  email,
+  password,
+}: loginOptins): Promise<AuthResponse> => {
+  try {
+    const { data } = await tesloApi.post<AuthResponse>("/auth/login", {
+      email: email,
+      password: password,
+    });
+
+    console.log({ data });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
