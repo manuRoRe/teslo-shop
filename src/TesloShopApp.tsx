301b8242a471c258e3febcc38f57acb442f39263
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-19 14:40:35 +0100

    Add sonner for toast notifications and implement login functionality

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -3,13 +3,14 @@ import { appRouter } from "./app.router";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* The rest of your application */}
+      <Toaster />
       <RouterProvider router={appRouter}></RouterProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
